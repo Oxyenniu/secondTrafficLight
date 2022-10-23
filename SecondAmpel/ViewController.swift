f78d4b20a17641ac4a494b0966024c605d60962f
Oxyenniu <Oxyenniu@gmail.com>
2022-10-23 12:37:43 +0300

    Initial Commit

---
+++ SecondAmpel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondAmpel
+//
+//  Created by Oxyenniu on 23.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
