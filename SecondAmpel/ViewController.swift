31dad80997b1e5747a43f5ea0d5a72d7bb63f9a4
Oxyenniu <Oxyenniu@gmail.com>
2022-10-23 15:33:25 +0300

    Setup UI

--- SecondAmpel/ViewController.swift
+++ SecondAmpel/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLightView: UIView!
+    
+    @IBOutlet var yellowLightView: UIView!
+    
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        redLightView.layer.cornerRadius = 55
+        yellowLightView.layer.cornerRadius = 55
+        greenLightView.layer.cornerRadius = 55
+        
+        startButton.layer.cornerRadius = 10
+        
+        
     }
 
-
+    
+    @IBAction func startButtonTap() {
+        
+        if redLightView.alpha == 0.3 || yellowLightView.alpha == 0.3 || greenLightView.alpha == 0.3 {
+            startButton.setTitle("START", for: .normal)
+        } else {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+    }
+    
 }
 
